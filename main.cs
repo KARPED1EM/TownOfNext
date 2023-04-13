efa688e4d83a4d81f5667f9f2b445dba2f7afa9c
KARPED1EM <leever.zzz@gmail.com>
2023-04-13 10:54:41 +0800

    Update Version

--- main.cs
+++ main.cs
@@ -35,7 +35,7 @@ public class Main : BasePlugin
     public static readonly string BANNEDWORDS_FILE_PATH = "./TOHE_DATA/BanWords.txt";
     public const string PluginGuid = "com.karped1em.townofhostedited";
     public const string PluginVersion = "2.2.8";
-    public const int PluginCreate = 19;
+    public const int PluginCreate = 20;
 
     public static readonly bool ShowQQButton = true;
     public static readonly string QQInviteUrl = "https://jq.qq.com/?_wv=1027&k=2RpigaN6";
