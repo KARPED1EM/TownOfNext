00ec8102b49f71c0f82d33b2b4e582da8577ba7b
KARPED1EM <leever.zzz@gmail.com>
2023-04-13 10:51:36 +0800

    Version Check Fix

--- Modules/ModUpdater.cs
+++ Modules/ModUpdater.cs
@@ -104,6 +104,7 @@ public class ModUpdater
             if (CultureInfo.CurrentCulture.Name.StartsWith("zh")) notice = notices[0];
             else notice = notices[1];
             int create = int.Parse(data[0]);
+            if (create.ToString().Length == 7) create = int.Parse(data[0][..2]);
             MD5 = data[2];
             visit = int.TryParse(data[3], out int x) ? x : 0;
             visit += 26810; //1x版本的访问量
