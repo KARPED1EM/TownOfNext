3b385100a4d507acad5c635198d280acca82f6d2
KARPED1EM <leever.zzz@gmail.com>
2023-04-17 19:25:24 +0800

    Fix NameNotify Host Check

--- Modules/NameNotifyManager.cs
+++ Modules/NameNotifyManager.cs
@@ -40,6 +40,7 @@ public static class NameNotifyManager
     }
     private static void SendRPC(byte playerId)
     {
+        if (!AmongUsClient.Instance.AmHost) return;
         MessageWriter writer = AmongUsClient.Instance.StartRpcImmediately(PlayerControl.LocalPlayer.NetId, (byte)CustomRPC.SyncNameNotify, SendOption.Reliable, -1);
         writer.Write(playerId);
         if (Notice.ContainsKey(playerId))
