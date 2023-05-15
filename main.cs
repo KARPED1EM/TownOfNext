508eedc2b855c8f63b0f306bddedb5a3974d1ee2
KARPED1EM <leever.zzz@gmail.com>
2023-05-15 13:17:52 +0800

    Update Version

--- main.cs
+++ main.cs
@@ -33,7 +33,7 @@ public class Main : BasePlugin
     public static ConfigEntry<string> DebugKeyInput { get; private set; }
     public static readonly string MainMenuText = "哟这不TOHE吗，几天不见Bug又多了？";
     public const string PluginGuid = "com.karped1em.townofhostedited";
-    public const string PluginVersion = "2.3.4";
+    public const string PluginVersion = "2.3.5";
     public const int PluginCreate = 5;
 
     public static readonly bool ShowQQButton = true;
