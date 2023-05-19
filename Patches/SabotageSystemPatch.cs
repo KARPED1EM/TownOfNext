141c608ed9b63e050a42c861f048d00e01dcf502
Tommy XL <fadeevvladimir16@gmail.com>
2023-05-19 11:13:53 +0800

    Code Cleanup

--- Patches/SabotageSystemPatch.cs
+++ Patches/SabotageSystemPatch.cs
@@ -1,5 +1,4 @@
 using HarmonyLib;
-using TOHE.Roles.Impostor;
 
 namespace TOHE;
 
--- Patches/ShipStatusPatch.cs
+++ Patches/ShipStatusPatch.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using TOHE.Roles.Crewmate;
-using TOHE.Roles.Impostor;
 using TOHE.Roles.Neutral;
 using UnityEngine;
 
