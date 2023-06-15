f589f76f992878f016af2772a1ad3239d4bee584
KARPED1EM <leever.zzz@gmail.com>
2023-06-16 02:12:18 +0800

    CreditsScreenPatch

--- Patches/CreditsScreenPatch.cs
+++ Patches/CreditsScreenPatch.cs
@@ -1,201 +1,120 @@
-﻿using BepInEx.Unity.IL2CPP.Utils;
-using HarmonyLib;
-using System.Collections;
+﻿using HarmonyLib;
 using System.Collections.Generic;
-using System.Linq;
 using TMPro;
 using UnityEngine;
 using static TOHE.Translator;
 
 namespace TOHE;
 
-/*
-[HarmonyPatch(typeof(CreditsScreenPopUp), nameof(CreditsScreenPopUp.))]
-internal class CreditsScreenPopUpAwakePatch
+[HarmonyPatch(typeof(CreditsScreenPopUp))]
+public class CreditsScreenPatch
 {
-    private static List<CreditsScreenPopUp.CreditsBlock> TOHECreditsBlocks;
-    public static void Postfix(CreditsScreenPopUp __instance)
+    private static GameObject TOHELogo;
+    [HarmonyPatch(nameof(CreditsScreenPopUp.OnEnable)), HarmonyPrefix]
+    public static void Prefix(CreditsScreenPopUp __instance)
     {
-        Logger.Test("LoadScene");
-        if (__instance == null || __instance.allCredits == null) return;
-        __instance.allCredits ??= new();
-        if (TOHECreditsBlocks == null) InitCreditsBlocks();
-        TOHECreditsBlocks.Where(x => !__instance.allCredits.Contains(x)).Do(__instance.allCredits.Add);
-        Logger.Test("-------- Added ---------");
-
-        return;
-    }
-    public static void InitCreditsBlocks()
-    {
-        TOHECreditsBlocks = new();
-
-        static CreditsScreenPopUp.CreditsLine NewLine(string line)
-            => new() { Title = line };
-
-        CreditsScreenPopUp.CreditsBlock devs = new()
-        { Header = GetString("Developer"), Lines = new() };
-        
-        devs.Lines.Add(NewLine("KARPED1EM"));
-        devs.Lines.Add(NewLine("IRIDESCENT"));
-        devs.Lines.Add(NewLine("Endrmen40409"));
-        devs.Lines.Add(NewLine("NCSIMON"));
-        devs.Lines.Add(NewLine("天寸梦初"));
-        devs.Lines.Add(NewLine("Commandf1"));
-        devs.Lines.Add(NewLine("喜"));
-        devs.Lines.Add(NewLine("SHAAARKY"));
-        TOHECreditsBlocks.Add(devs);
-
-    }
-}
-*/
-
-//参考：https://github.com/ykundesu/SuperNewRoles/blob/master/SuperNewRoles/Patches/LogoAndStampPatch.cs
-[HarmonyPatch]
-public static class LogoAndStampPatch
-{
-    public static GenericPopup popup;
-
-    [HarmonyPatch(typeof(MainMenuManager), nameof(MainMenuManager.Start))]
-    public static class LogoPatch
-    {
-        static IEnumerator ViewCredentialsCoro(MainMenuManager __instance)
+        if (TOHELogo == null)
         {
-            while (true)
+            var devsList = new List<string>()
             {
-                yield return new WaitForSeconds(1f);
-                if (__instance != null)
-                {
-                    ViewBoosterPatch(__instance);
-                }
-                break;
+                $"<color={Main.ModColor}>♡KARPED1EM</color> - {GetString("Creater")}",
+                $"<color={Main.ModColor}>♡IRIDESCENT</color> - {GetString("Art")}",
+                $"SHAAARKY - {GetString("Developer")}",
+                $"Endrmen40409 - {GetString("Art")}",
+                $"天寸梦初 - {GetString("Developer")}",
+                $"NCSIMON - {GetString("Developer")}",
+                $"Commandf1 - {GetString("Developer")}",
+                $"喜 - {GetString("Developer")}",
+            };
+            var translatorsList = new List<string>()
+            {
+                $"Tommy-XL - {GetString("TranEN")}&{GetString("TranRU")}",
+                $"Tem - {GetString("TranEN")}&{GetString("TranRU")}",
+                $"阿龍 - {GetString("TranCHT")}",
+                $"Gurge44 - {GetString("TranEN")}",
+                $"法官 - {GetString("TranCHT")}",
+                $"SolarFlare - {GetString("TranEN")}",
+                $"chill_ultimated - {GetString("TranRU")}"
+            };
+            var acList = new List<string>()
+            {
+                $"罗寄 - {GetString("Sponsor")}",
+                $"鬼 - {GetString("Sponsor")}",
+                $"喜 - {GetString("Sponsor")}",
+                $"小叨院长 - {GetString("Sponsor")}",
+                $"波奇酱 - {GetString("Sponsor")}",
+                $"法师 - {GetString("Sponsor")}",
+                $"沐煊 - {GetString("Sponsor")}",
+                $"SolarFlare - {GetString("Sponsor")}",
+                $"林林林 - {GetString("Sponsor")}",
+                $"撒币 - {GetString("Sponsor")}",
+                $"斯卡蒂Skadi - {GetString("Sponsor")}",
+                $"ltemten - {GetString("Sponsor")}",
+                $"Night_瓜 - {GetString("Sponsor")}",
+                $"群诱饵 - {GetString("Sponsor")}",
+                $"Slok - {GetString("Sponsor")}",
+                $"辣鸡 - {GetString("Sponsor")}",
+                $"湛蓝色 - {GetString("Sponsor")}",
+                $"小黄117 - {GetString("Sponsor")}",
+                $"chun - {GetString("Sponsor")}",
+                $"Z某 - {GetString("Sponsor")}",
+                $"Shark - {GetString("Sponsor")}",
+                $"清风awa - {GetString("Sponsor")}",
+                $"1 1 1 1 - {GetString("Sponsor")}",
+
+                $"bunny - {GetString("Booster")}",
+                $"Loonie - {GetString("Booster")}",
+                $"Namra - {GetString("Booster")}",
+                $"KNIGHT - {GetString("Booster")}",
+                $"SolarFlare - {GetString("Booster")}",
+                $"Bluéfôx. - {GetString("Booster")}",
+                $"shiftyrose - {GetString("Booster")}",
+                $"M ™ - {GetString("Booster")}",
+                $"yunfi - {GetString("Booster")}",
+            };
+
+            var template = GameObject.Find("logoImage");
+            TOHELogo = Object.Instantiate(template, __instance.CreditsParent.transform);
+            TOHELogo.name = "TOHE Logo Image";
+            TOHELogo.transform.localPosition = template.transform.localPosition;
+            var logoRenderer = TOHELogo.GetComponent<SpriteRenderer>();
+            logoRenderer.sprite = Utils.LoadSprite("TOHE.Resources.Images.TOHE-Icon.png", 76f);
+
+            GameObject CreateBlock(string header, List<string> lines)
+            {
+                var template = __instance.CreditsParent.transform.GetChild(2).gameObject;
+                var block = Object.Instantiate(template, __instance.CreditsParent.transform);
+                block.name = $"TOHE CreditsBlock";
+                string content = "";
+                lines.Do(l => content += "\n" + l);
+                block.transform.FindChild("Header").GetComponent<TextMeshPro>().SetText(header);
+                var tmp = block.transform.FindChild("CreditsLines").GetComponent<TextMeshPro>();
+                tmp.fontSizeMax = tmp.fontSizeMin = 1.9f;
+                tmp.SetText(content.TrimStart('\n'));
+                return block;
             }
-        }
-        public static string BoosterData = "";
-        public static string SponsersData = "";
-        public static string DevsData = "";
-        public static string TransData = "";
 
-        public static void InitCredentialsData()
-        {
-            BoosterData = "";
-            SponsersData = "";
-            DevsData = "";
-            TransData = "";
+            float offset = 1f;
 
-            DevsData += $"<color={Main.ModColor}>♡咔皮呆</color> - <size=75%>{GetString("MainDev")}</size>";
-            DevsData += $"\n<color={Main.ModColor}>♡IRIDESCENT</color> - <size=75%>{GetString("Art")}</size>";
-            DevsData += $"\nEndrmen40409 - <size=75%>{GetString("Art")}</size>";
-            DevsData += $"\nNCSIMON - <size=75%>{GetString("RoleDev")}</size>";
-            DevsData += $"\n天寸梦初 - <size=75%>{GetString("RoleDev")}&{GetString("TechSup")}</size>";
-            DevsData += $"\nCommandf1 - <size=75%>{GetString("RoleDev")}&{GetString("FeatureDev")}</size>";
-            DevsData += $"\n喜 - <size=75%>{GetString("RoleDev")}</size>";
-            DevsData += $"\nSHAAARKY - <size=75%>{GetString("RoleDev")}</size>";
+            var Devs = CreateBlock("", devsList);
+            Devs.transform.localPosition = TOHELogo.transform.localPosition - new Vector3(0f, offset, 0f);
+            offset += 0.35f * devsList.Count + 1f;
 
-            TransData += $"Tommy-XL - <size=75%>{GetString("TranEN")}&{GetString("TranRU")}</size>";
-            TransData += $"\nTem - <size=75%>{GetString("TranEN")}&{GetString("TranRU")}</size>";
-            TransData += $"\n阿龍 - <size=75%>{GetString("TranCHT")}</size>";
-            TransData += $"\nGurge44 - <size=75%>{GetString("TranEN")}</size>";
-            TransData += $"\n法官 - <size=75%>{GetString("TranCHT")}</size>";
-            TransData += $"\nSolarFlare - <size=75%>{GetString("TranEN")}</size>";
-            TransData += $"\nchill_ultimated - <size=75%>{GetString("TranRU")}</size>";
+            var Translators = CreateBlock(GetString("Translator"), translatorsList);
+            Translators.transform.localPosition = TOHELogo.transform.localPosition - new Vector3(0f, offset, 0f);
+            offset += 0.35f * translatorsList.Count + 1f;
 
-            BoosterData += $"bunny";
-            BoosterData += $"\nLoonie";
-            BoosterData += $"\nNamra";
-            BoosterData += $"\nKNIGHT";
-            BoosterData += $"\nSolarFlare";
-            BoosterData += $"\nBluéfôx.";
-            BoosterData += $"\nshiftyrose";
-            BoosterData += $"\nM ™";
-            BoosterData += $"\nyunfi";
+            var Acknowledgements = CreateBlock(GetString("Acknowledgement"), acList);
+            Acknowledgements.transform.localPosition = TOHELogo.transform.localPosition - new Vector3(0f, offset, 0f);
+            offset += 0.35f * acList.Count + 3f;
 
-            SponsersData += $"罗寄";
-            SponsersData += $"\n鬼";
-            SponsersData += $"\n喜";
-            SponsersData += $"\n小叨院长";
-            SponsersData += $"\n波奇酱";
-            SponsersData += $"\n法师";
-            SponsersData += $"\n沐煊";
-            SponsersData += $"\nSolarFlare";
-            SponsersData += $"\n林林林";
-            SponsersData += $"\n撒币";
-            SponsersData += $"\n斯卡蒂Skadi";
-            SponsersData += $"\nltemten";
-            SponsersData += $"\n\n<size=60%>({GetString("OnlyShowPart")})</size>";
+            __instance.CreditsParent.ForEachChild((Il2CppSystem.Action<GameObject>)Move);
+            void Move(GameObject obj)
+            {
+                if (obj.name.StartsWith("TOHE")) return;
+                obj.transform.localPosition -= new Vector3(0f, offset, 0f);
+            }
 
-        }
-
-        public static GameObject CreditsPopup;
-        static void ViewBoosterPatch(MainMenuManager __instance)
-        {
-            var template = __instance.transform.FindChild("StatsPopup");
-            var obj = Object.Instantiate(template, template.transform.parent).gameObject;
-            CreditsPopup = obj;
-            Object.Destroy(obj.GetComponent<StatsPopup>());
-
-            var devtitletext = obj.transform.FindChild("StatNumsText_TMP");
-            devtitletext.GetComponent<TextMeshPro>().text = GetString("Developer");
-            devtitletext.GetComponent<TextMeshPro>().alignment = TextAlignmentOptions.Center;
-            devtitletext.localPosition = new Vector3(-2.4f, 1.65f, -2f);
-            devtitletext.localScale = new Vector3(0.8f, 0.8f, 0.8f);
-
-            var devtext = obj.transform.FindChild("StatsText_TMP");
-            devtext.GetComponent<TextMeshPro>().text = DevsData;
-            devtext.GetComponent<TextMeshPro>().alignment = TextAlignmentOptions.Capline;
-            devtext.localPosition = new Vector3(-2.4f, 1.27f, -2f);
-            devtext.localScale = new Vector3(0.5f, 0.5f, 1f);
-
-            var transtitletext = Object.Instantiate(devtitletext, obj.transform);
-            transtitletext.GetComponent<TextMeshPro>().text = GetString("Translator");
-            transtitletext.GetComponent<TextMeshPro>().alignment = TextAlignmentOptions.Center;
-            transtitletext.localPosition = new Vector3(0f, 1.65f, -2f);
-            transtitletext.localScale = new Vector3(0.8f, 0.8f, 1f);
-
-            var transtext = Object.Instantiate(devtext, obj.transform);
-            transtext.GetComponent<TextMeshPro>().text = TransData;
-            transtext.GetComponent<TextMeshPro>().alignment = TextAlignmentOptions.Capline;
-            transtext.localPosition = new Vector3(0f, 1.27f, -2f);
-            transtext.localScale = new Vector3(0.5f, 0.5f, 1f);
-
-            var boostertitletext = Object.Instantiate(devtitletext, obj.transform);
-            boostertitletext.GetComponent<TextMeshPro>().text = GetString("Booster");
-            boostertitletext.GetComponent<TextMeshPro>().alignment = TextAlignmentOptions.Center;
-            boostertitletext.localPosition = new Vector3(-2.4f, -0.7f, -2f);
-            boostertitletext.localScale = new Vector3(0.8f, 0.7f, 1f);
-
-            var boostertext = Object.Instantiate(devtext, obj.transform);
-            boostertext.GetComponent<TextMeshPro>().text = BoosterData;
-            boostertext.GetComponent<TextMeshPro>().alignment = TextAlignmentOptions.Capline;
-            boostertext.localPosition = new Vector3(-2.4f, -0.98f, -2f);
-            boostertext.localScale = new Vector3(0.5f, 0.5f, 1f);
-
-            var sponsortitletext = Object.Instantiate(devtitletext, obj.transform);
-            sponsortitletext.GetComponent<TextMeshPro>().text = GetString("Sponsor");
-            sponsortitletext.GetComponent<TextMeshPro>().alignment = TextAlignmentOptions.Center;
-            sponsortitletext.localPosition = new Vector3(2.4f, 1.65f, -2f);
-            sponsortitletext.localScale = new Vector3(0.8f, 0.8f, 1f);
-
-            var sponsortext = Object.Instantiate(devtext, obj.transform);
-            sponsortext.GetComponent<TextMeshPro>().text = SponsersData;
-            sponsortext.GetComponent<TextMeshPro>().alignment = TextAlignmentOptions.Capline;
-            sponsortext.localPosition = new Vector3(2.4f, 1.27f, -2f);
-            sponsortext.localScale = new Vector3(0.5f, 0.5f, 1f);
-
-            var textobj = obj.transform.FindChild("Title_TMP");
-            Object.Destroy(textobj.GetComponent<TextTranslatorTMP>());
-            textobj.GetComponent<TextMeshPro>().text = GetString("DevAndSpnTitle");
-            textobj.GetComponent<TextMeshPro>().alignment = TextAlignmentOptions.Center;
-            textobj.localScale = new Vector3(1.2f, 1.2f, 1f);
-            textobj.localPosition = new Vector3(0f, 2.2f, -2f);
-            obj.transform.FindChild("Background").localScale = new Vector3(1.5f, 1f, 1f);
-            obj.transform.FindChild("CloseButton").localPosition = new Vector3(-3.75f, 2.65f, 0);
-        }
-        public static MainMenuManager instance;
-        public static void Postfix(MainMenuManager __instance)
-        {
-            InitCredentialsData();
-            instance = __instance;
-            AmongUsClient.Instance.StartCoroutine(ViewCredentialsCoro(__instance));
         }
     }
 }
