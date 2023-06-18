e711ade53fa66f0aa889922453546bfa7ca1772a
KARPED1EM <leever.zzz@gmail.com>
2023-06-18 20:42:38 +0800

    Fix Text Box Limit

--- Patches/TextBoxPatch.cs
+++ Patches/TextBoxPatch.cs
@@ -5,8 +5,8 @@ namespace TOHE;
 [HarmonyPatch(typeof(TextBoxTMP))]
 public class TextBoxPatch
 {
-    [HarmonyPatch(nameof(TextBoxTMP.IsCharAllowed)), HarmonyPostfix]
-    public static void IsCharAllowed(TextBoxTMP __instance, char i, ref bool __result) => __result = i > 31 && i is not ('\r' or '\n');
+    //[HarmonyPatch(nameof(TextBoxTMP.IsCharAllowed)), HarmonyPostfix]
+    //public static void IsCharAllowed(TextBoxTMP __instance, char i, ref bool __result) => __result &= i is not ('\r' or '\n');
     [HarmonyPatch(nameof(TextBoxTMP.SetText)), HarmonyPrefix]
     public static void ModifyCharacterLimit(TextBoxTMP __instance) => __instance.characterLimit = AmongUsClient.Instance.AmHost ? 999 : 300;
 }
