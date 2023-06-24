dc88ef4b6b63a515ae521060ea3f19c638c9a3d8
KARPED1EM <leever.zzz@gmail.com>
2023-06-24 15:37:10 +0800

    Prevent pointless movement of RightPanel animation

--- TOHE/Patches/MainMenuManagerPatch.cs
+++ TOHE/Patches/MainMenuManagerPatch.cs
@@ -39,7 +39,10 @@ public class MainMenuManagerPatch
 
         var pos1 = TitleLogoPatch.RightPanel.transform.localPosition;
         Vector3 lerp1 = Vector3.Lerp(pos1, TitleLogoPatch.RightPanelOp + new Vector3((ShowingPanel ? 0f : 10f), 0f, 0f), Time.deltaTime * (ShowingPanel ? 3f : 2f));
-        TitleLogoPatch.RightPanel.transform.localPosition = lerp1;
+        if (ShowingPanel 
+            ? TitleLogoPatch.RightPanel.transform.localPosition.x > TitleLogoPatch.RightPanelOp.x + 0.03f
+            : TitleLogoPatch.RightPanel.transform.localPosition.x < TitleLogoPatch.RightPanelOp.x + 9f
+            ) TitleLogoPatch.RightPanel.transform.localPosition = lerp1;
 
         if (ShowedBak || !isOnline) return;
         var bak = GameObject.Find("BackgroundTexture");
