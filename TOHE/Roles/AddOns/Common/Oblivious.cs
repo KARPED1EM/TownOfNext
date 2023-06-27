7994673d90b2fd06a6153e9bb531c6c923141633
KARPED1EM <leever.zzz@gmail.com>
2023-06-28 00:01:50 +0800

    Add Oblivious

---
+++ TOHE/Roles/AddOns/Common/Oblivious.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System.Linq;
+using TOHE.Roles.Core;
+using UnityEngine;
+using static TOHE.Options;
+
+namespace TOHE.Roles.AddOns.Common;
+public static class Oblivious
+{
+    private static readonly int Id = 81100;
+    private static Color RoleColor = Utils.GetRoleColor(CustomRoles.Oblivious);
+    private static List<byte> playerIdList = new();
+
+    public static void SetupCustomOption()
+    {
+        SetupAddonOptions(Id, TabGroup.Addons, CustomRoles.Oblivious);
+        AddOnsAssignData.Create(Id + 10, CustomRoles.Oblivious, true, true, false);
+    }
+    public static void Init()
+    {
+        playerIdList = new();
+    }
+    public static void Add(byte playerId)
+    {
+        playerIdList.Add(playerId);
+    }
+    public static bool IsEnable => playerIdList.Count > 0;
+    public static bool IsThisRole(byte playerId) => playerIdList.Contains(playerId);
+}
