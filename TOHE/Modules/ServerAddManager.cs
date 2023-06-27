e919cea6405af2a4e1c7724f25add0c8f741223e
MC_huier_lgsn <107238739+MC-AS-Huier@users.noreply.github.com>
2023-06-27 12:55:52 +0800

    Remove Futility Server

--- TOHE/Modules/ServerAddManager.cs
+++ TOHE/Modules/ServerAddManager.cs
@@ -13,21 +13,17 @@ public static class ServerAddManager
     private static ServerManager serverManager = DestroyableSingleton<ServerManager>.Instance;
     public static void Init()
     {
-        
+
         serverManager.AvailableRegions = ServerManager.DefaultRegions;
         List<IRegionInfo> regionInfos = new();
 
         if (CultureInfo.CurrentCulture.Name.StartsWith("zh"))
         {
             regionInfos.Add(CreateHttp("au-sh.pafyx.top", "梦服上海 (新)", 22000, false));
-            regionInfos.Add(CreateHttp("103.151.216.200", "梦服香港 (新)", 22000, false));
             regionInfos.Add(CreateHttp("124.222.148.195", "小猫私服", 22000, false));
-            regionInfos.Add(CreateHttp("103.151.216.200", "新港服", 22000, false));
-            regionInfos.Add(CreateHttp("47.122.47.172", "梦服福州", 22000, false));
-            //regionInfos.Add(CreateHttp("120.78.171.61", "霸总广州", 22000, false));
         }
         regionInfos.Add(CreateHttp("au-as.duikbo.at", "Modded Asia (MAS)", 443, true));
-        regionInfos.Add(CreateHttp("aumods.one", "Modded NA (MNA)", 443, true));
+        regionInfos.Add(CreateHttp("www.aumods.xyz", "Modded NA (MNA)", 443, true));
         regionInfos.Add(CreateHttp("au-eu.duikbo.at", "Modded EU (MEU)", 443, true));
 
         var defaultRegion = serverManager.CurrentRegion;
@@ -40,26 +36,6 @@ public static class ServerAddManager
         string serverIp = (ishttps ? "https://" : "http://") + ip;
         ServerInfo serverInfo = new ServerInfo(name, serverIp, port, false);
         ServerInfo[] ServerInfo = new ServerInfo[] { serverInfo };
-        return new StaticHttpRegionInfo(name, (StringNames)1003, ip, ServerInfo).CastFast<IRegionInfo>();
+        return new StaticHttpRegionInfo(name, (StringNames)1003, ip, ServerInfo).Cast<IRegionInfo>();
     }
-
-    private static class CastHelper<T> where T : Il2CppObjectBase
-    {
-        public static Func<IntPtr, T> Cast;
-        static CastHelper()
-        {
-            var constructor = typeof(T).GetConstructor(new[] { typeof(IntPtr) });
-            var ptr = Expression.Parameter(typeof(IntPtr));
-            var create = Expression.New(constructor!, ptr);
-            var lambda = Expression.Lambda<Func<IntPtr, T>>(create, ptr);
-            Cast = lambda.Compile();
-        }
-    }
-
-    private static T CastFast<T>(this Il2CppObjectBase obj) where T : Il2CppObjectBase
-    {
-        if (obj is T casted) return casted;
-        return CastHelper<T>.Cast(obj.Pointer);
-    }
-
 }
