7e0708fc2fa736ac0c42036114e517ef571d19fa
KARPED1EM <leever.zzz@gmail.com>
2023-10-13 02:23:56 +0800

    Change Ladder  Fall Task Delay 0.05f => 0.30f

--- TONX/Patches/LadderPatch.cs
+++ TONX/Patches/LadderPatch.cs
@@ -58,7 +58,7 @@ public class FallFromLadder
                     var state = PlayerState.GetByPlayerId(player.PlayerId);
                     state.DeathReason = CustomDeathReason.Fall;
                     state.SetDead();
-                }, 0.05f, "LadderFallTask");
+                }, 0.30f, "LadderFallTask");
             }
         }
     }
