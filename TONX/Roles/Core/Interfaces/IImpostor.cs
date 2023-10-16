7a9fefa08752134b97e8914ff3005d9f9f23b9a9
KARPED1EM <leever.zzz@gmail.com>
2023-10-16 11:25:52 +0800

    Add ISchrodingerCatOwner To IImp

--- TONX/Roles/Core/Interfaces/IImpostor.cs
+++ TONX/Roles/Core/Interfaces/IImpostor.cs
@@ -1,10 +1,12 @@
+using AmongUs.GameOptions;
+
 namespace TONX.Roles.Core.Interfaces;
 
 /// <summary>
 /// 内鬼的接口<br/>
 /// <see cref="IKiller"/>的继承
 /// </summary>
-public interface IImpostor : IKiller
+public interface IImpostor : IKiller, ISchrodingerCatOwner
 {
     /// <summary>
     /// 是否可以成为绝境者
@@ -15,4 +17,19 @@ public interface IImpostor : IKiller
     /// 可以作为内鬼使用通风管
     /// </summary>
     public bool CanUseImpostorVentButton => true;
+
+    /// <summary>
+    /// 击杀猫猫的时候职业发生了变化<br/>
+    /// デフォルト<see cref="SchrodingerCat.TeamType.TeamImpostor"/>
+    /// </summary>
+    SchrodingerCat.TeamType ISchrodingerCatOwner.SchrodingerCatChangeTo => SchrodingerCat.TeamType.Mad;
+
+    /// <summary>
+    /// 该位置的选项改为猫猫的设置<br/>
+    /// デフォルト<see cref="SchrodingerCat.ApplyMadCatOptions"/>
+    /// </summary>
+    void ISchrodingerCatOwner.ApplySchrodingerCatOptions(IGameOptions option)
+    {
+        SchrodingerCat.ApplyMadCatOptions(option);
+    }
 }
