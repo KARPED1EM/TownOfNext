507c0bc7a19876d5971da984359b337478694d46
KARPED1EM <leever.zzz@gmail.com>
2023-10-23 23:14:54 +0800

    Fix Imp Shows Admin Map After Death

--- TONX/Patches/MapRealTimeLocationPatch.cs
+++ TONX/Patches/MapRealTimeLocationPatch.cs
@@ -16,7 +16,7 @@ public class MapRealTimeLocationPatch
     [HarmonyPatch(typeof(MapBehaviour), nameof(MapBehaviour.ShowSabotageMap)), HarmonyPrefix]
     public static bool ShowSabotageMap(MapBehaviour __instance)
     {
-        if (!ShouldShowRealTime) return true;
+        if (!ShouldShowRealTime || PlayerControl.LocalPlayer.Is(Roles.Core.CustomRoleTypes.Impostor)) return true;
         __instance.ShowCountOverlay(true, true, true);
         return false;
     }
