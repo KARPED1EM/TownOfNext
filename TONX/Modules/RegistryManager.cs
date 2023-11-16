f332283b303bdf8c131d8fe275afcfd5d55f5750
KARPED1EM <leever.zzz@gmail.com>
2023-11-17 07:15:27 +0800

    Auto Delete Old Template File

--- TONX/Modules/RegistryManager.cs
+++ TONX/Modules/RegistryManager.cs
@@ -33,7 +33,7 @@ public static class RegistryManager
         Keys.SetValue("Last launched version", Main.version.ToString());
         Keys.SetValue("Path", Path.GetFullPath("./"));
 
-        List<string> FoldersToDel = new()
+        List<string> FoldersNFileToDel = new()
             {
                 @"./TOH_DATA",
                 @"./TOHE_DATA",
@@ -41,16 +41,28 @@ public static class RegistryManager
 
         Logger.Warn("上次启动的TONX版本：" + LastVersion, "Registry Manager");
 
-        if (LastVersion < new Version(1, 0, 0))
+        if (LastVersion < new Version(3, 0, 0))
         {
-            Logger.Warn("v1.0.0 New Version Operation Needed", "Registry Manager");
-            FoldersToDel.Add(@"./BepInEx/config");
+            Logger.Warn("v3.0.0 New Version Operation Needed", "Registry Manager");
+            FoldersNFileToDel.Add(@"./BepInEx/config");
         }
 
-        FoldersToDel.DoIf(Directory.Exists, p =>
+        if (LastVersion <= new Version(3, 0, 0))
+        {
+            Logger.Warn("v3.0.1 New Version Operation Needed", "Registry Manager");
+            FoldersNFileToDel.Add(@"./TONX/Data/template.txt");
+        }
+
+        FoldersNFileToDel.DoIf(Directory.Exists, p =>
         {
             Logger.Warn("Delete Useless Directory:" + p, "Registry Manager");
             Directory.Delete(p, true);
         });
+        FoldersNFileToDel.DoIf(File.Exists, p =>
+        {
+            Logger.Warn("Delete Useless File:" + p, "Registry Manager");
+            File.Delete(p);
+        });
+
     }
 }
