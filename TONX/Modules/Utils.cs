aa81a6354ded1565215dc9fd9d2f20674d3cb52d
KARPED1EM <leever.zzz@gmail.com>
2023-11-18 22:47:08 +0800

    Add BlackRoom Ps for Fungle

--- TONX/Modules/Utils.cs
+++ TONX/Modules/Utils.cs
@@ -1334,6 +1334,7 @@ public static class Utils
             1 => new(-11.4f, 8.2f), // MIRA HQ
             2 => new(42.6f, -19.9f), // Polus
             4 => new(-16.8f, -6.2f), // Airship
+            5 => new(9.4f, 17.9f), // The Fungle
             _ => throw new System.NotImplementedException(),
         };
     }
