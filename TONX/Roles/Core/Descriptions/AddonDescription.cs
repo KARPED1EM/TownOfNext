ba254dfb9f600b076ac03009a6c34ccc9b80b095
KARPED1EM <leever.zzz@gmail.com>
2023-11-22 12:28:19 +0800

    Add AddonDescription

---
+++ TONX/Roles/Core/Descriptions/AddonDescription.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace TONX.Roles.Core.Descriptions;
+
+public static class AddonDescription
+{
+    public static string FullFormatHelp(PlayerControl player)
+    {
+        var builder = new StringBuilder(512);
+        var subRoles = player.GetCustomSubRoles();
+        if (CustomRoles.Neptune.IsExist() && !subRoles.Contains(CustomRoles.Lovers) && !player.Is(CustomRoles.GM) && !player.Is(CustomRoles.Neptune))
+        {
+            subRoles.Add(CustomRoles.Lovers);
+        }
+
+        foreach (var subRole in subRoles)
+        {
+            if (subRoles.IndexOf(subRole) != 0) builder.AppendFormat("<size={0}>\n", BlankLineSize);
+            builder.AppendFormat("<size={0}>{1}\n", FirstHeaderSize, Translator.GetRoleString(subRole.ToString()).Color(Utils.GetRoleColor(subRole)));
+            builder.AppendFormat("<size={0}>{1}\n", BodySize, Translator.GetString($"{subRole}InfoLong"));
+        }
+
+        return builder.ToString();
+    }
+
+    public const string FirstHeaderSize = "130%";
+    public const string SecondHeaderSize = "100%";
+    public const string BodySize = "70%";
+    public const string BlankLineSize = "30%";
+}
--- TONX/Roles/Core/Descriptions/RoleDescription.cs
+++ TONX/Roles/Core/Descriptions/RoleDescription.cs
@@ -36,23 +36,13 @@ public abstract class RoleDescription
             return builder.ToString();
         }
     }
-    public string GetFullFormatHelpWithAddons(PlayerControl player)
+    public string GetMainRoleFullFormatHelpWithAddons(PlayerControl player)
     {
-        var builder = new StringBuilder(512);
+        var builder = new StringBuilder(1024);
+
         builder.Append(FullFormatHelp);
-
-        var subRoles = player.GetCustomSubRoles();
-        if (CustomRoles.Neptune.IsExist() && !subRoles.Contains(CustomRoles.Lovers) && !player.Is(CustomRoles.GM) && !player.Is(CustomRoles.Neptune))
-        {
-            subRoles.Add(CustomRoles.Lovers);
-        }
-
-        foreach (var subRole in subRoles)
-        {
-            builder.AppendFormat("<size={0}>\n", BlankLineSize);
-            builder.AppendFormat("<size={0}>{1}\n", FirstHeaderSize, Translator.GetRoleString(subRole.ToString()).Color(Utils.GetRoleColor(subRole)));
-            builder.AppendFormat("<size={0}>{1}\n", BodySize, Translator.GetString($"{subRole}InfoLong"));
-        }
+        builder.AppendFormat("<size={0}>\n", BlankLineSize);
+        builder.Append(AddonDescription.FullFormatHelp(player));
 
         return builder.ToString();
     }
