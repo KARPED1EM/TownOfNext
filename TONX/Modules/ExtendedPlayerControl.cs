dfd509b6ec0e7446914f8295a56ba94bfbb62633
KARPED1EM <leever.zzz@gmail.com>
2023-11-22 20:09:50 +0800

    Fix IsEaten Null Ref Bug

--- TONX/Modules/ExtendedPlayerControl.cs
+++ TONX/Modules/ExtendedPlayerControl.cs
@@ -683,7 +683,7 @@ static class ExtendedPlayerControl
     public static bool Is(this PlayerControl target, CustomRoleTypes type) { return target.GetCustomRole().GetCustomRoleTypes() == type; }
     public static bool Is(this PlayerControl target, RoleTypes type) { return target.GetCustomRole().GetRoleTypes() == type; }
     public static bool Is(this PlayerControl target, CountTypes type) { return target.GetCountTypes() == type; }
-    public static bool IsEaten(this PlayerControl target) => Pelican.IsEaten(target.PlayerId);
+    public static bool IsEaten(this PlayerControl target) => GameStates.IsInGame && Pelican.IsEaten(target.PlayerId);
     public static bool IsAlive(this PlayerControl target)
     {
         //ロビーなら生きている
