6b787ef8056360f1887403a9ac77f6f5c3b67a08
KARPED1EM <leever.zzz@gmail.com>
2023-11-23 01:34:27 +0800

    Update Version

--- TONX/main.cs
+++ TONX/main.cs
@@ -38,7 +38,7 @@ public class Main : BasePlugin
     // == 版本相关设定 / Version Config ==
     public const string LowestSupportedVersion = "2023.10.24";
     public static readonly bool IsPublicAvailableOnThisVersion = false;
-    public const string PluginVersion = "3.0.0";
+    public const string PluginVersion = "3.0.1";
     public const int PluginCreation = 1;
     // == 链接相关设定 / Link Config ==
     public static readonly bool ShowWebsiteButton = true;
